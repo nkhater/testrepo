f9b3eef677cdb095adedce48a6323ddbf434253b
nkhater <nohakhater@gmail.com>
2019-07-05 13:52:11 +0200

    Added findings

--- jabref/logic/bibtex/BibEntryWriter.java
+++ jabref/logic/bibtex/BibEntryWriter.java
@@ -23,8 +23,9 @@ import org.jabref.model.strings.StringUtil;
 
 public class BibEntryWriter {
 
+    // TODO
     private final LatexFieldFormatter fieldFormatter;
-    private final boolean write;
+    public final boolean write;
 
 
     public BibEntryWriter(LatexFieldFormatter fieldFormatter, boolean write) {
@@ -163,7 +164,7 @@ public class BibEntryWriter {
             try {
                 out.write(fieldFormatter.format(field.get(), name));
                 out.write(',' + OS.NEWLINE);
-            } catch (InvalidFieldValueException ex) {
+            } catch (Exception ex) {
                 throw new IOException("Error in field '" + name + "': " + ex.getMessage(), ex);
             }
         }
