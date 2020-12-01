996d7e03679df24b4fe9a288e82dafb64e08cf21
nkhater <nohakhater@gmail.com>
2020-12-01 14:40:20 +0100

    Removed ToDos

--- jabref/Globals.java
+++ jabref/Globals.java
@@ -30,9 +30,6 @@ import com.microsoft.applicationinsights.internal.shutdown.SDKShutdownActivity;
 import com.microsoft.applicationinsights.telemetry.SessionState;
 
 public class Globals {
-	
-	// TODO Another line
-	// TODO Added a ToDo
 
     // JabRef version info
     public static final BuildInfo BUILD_INFO = new BuildInfo();
